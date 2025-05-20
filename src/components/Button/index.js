2490f28c8f5bbbfe6904d3cc322c8093d78306d0
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-05-19 23:50:20 -0300

    fix(component): imported styles and created conditions and variables

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,13 +4,17 @@ import {
     Text,
     TouchableHighlight
 } from 'react-native';
+import { style } from './styles';
 
 export default props => {
     const stylesButton = [style.button]
+    if(props.double) stylesButton.push(style.buttonDouble)
+    if(props.triple) stylesButton.push(style.buttonTriple)
+    if(props.operation) stylesButton.push(style.operationButton)
 
     return (
         <TouchableHighlight onPress={props.onClick}>
-            <Text style={style.button}>
+            <Text style={stylesButton}>
                 {props.label}
             </Text>
         </TouchableHighlight>
